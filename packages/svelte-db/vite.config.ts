248180a8553587d0d99d8b76e54cd726b24f052b
wobsoriano <sorianorobertc@gmail.com>
2025-05-17 06:57:50 -0700

    chore: update vite config

--- packages/svelte-db/vite.config.ts
+++ packages/svelte-db/vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     },
     typecheck: { enabled: true },
   },
+  // Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
   resolve: process.env.VITEST
     ? {
         conditions: [`browser`],
