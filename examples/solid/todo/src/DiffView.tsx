a00ce5c8b34da2a66f9b49befaa92acad29d0454
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2025-05-18 19:38:35 +0330

    feat: add todo example for solidjs.

---
+++ examples/solid/todo/src/DiffView.tsx
@@ -0,0 +1,35 @@
+import { diffLines } from "diff"
+import { For } from "solid-js"
+
+interface DiffViewProps {
+  oldValue: unknown
+  newValue: unknown
+}
+
+export function DiffView({ oldValue, newValue }: DiffViewProps) {
+  const diff = diffLines(
+    JSON.stringify(oldValue, null, 2),
+    JSON.stringify(newValue, null, 2)
+  )
+
+  return (
+    <pre class="text-sm font-mono bg-gray-50 p-2 rounded overflow-auto">
+      <For each={diff}>
+        {(part) => (
+          <div
+            class={`${
+              part.added
+                ? `bg-green-50 text-green-800 border-l-4 border-green-500`
+                : part.removed
+                  ? `bg-red-50 text-red-800 border-l-4 border-red-500`
+                  : `text-gray-800`
+            } px-2 whitespace-pre`}
+          >
+            {part.added ? `+ ` : part.removed ? `- ` : `  `}
+            {part.value}
+          </div>
+        )}
+      </For>
+    </pre>
+  )
+}
